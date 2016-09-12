4194136dee409acdeadbe9c6e7eeaf611ab2e0e5
Galdin Raphael <gldraphael@gmail.com>
2016-09-13 00:27:14 +0530

    Format lesson 1 code

--- 01/main.cpp
+++ 01/main.cpp
@@ -6,7 +6,7 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main( int argc, char* args[] )
+int main(int argc, char* args[])
 {
     //The window we'll be rendering to
     SDL_Window* window = NULL;
@@ -15,36 +15,44 @@ int main( int argc, char* args[] )
     SDL_Surface* screenSurface = NULL;
 
     //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
     }
 
     else
     {
         //Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( window == NULL )
+        window = SDL_CreateWindow(
+            "SDL Tutorial",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            SCREEN_WIDTH,
+            SCREEN_HEIGHT,
+            SDL_WINDOW_SHOWN
+        );
+
+        if(window == NULL)
         {
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         }
         else
         {
             //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
+            screenSurface = SDL_GetWindowSurface(window);
 
             //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
             
             //Update the surface
-            SDL_UpdateWindowSurface( window );
+            SDL_UpdateWindowSurface(window);
 
             //Wait two seconds
-            SDL_Delay( 2000 );
+            SDL_Delay(2000);
         }
     }
     //Destroy window
-    SDL_DestroyWindow( window );
+    SDL_DestroyWindow(window);
 
     //Quit SDL subsystems
     SDL_Quit();
