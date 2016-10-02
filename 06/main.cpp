3ca67ae86b7ded62fc674524ea48bd1c3537cb74
Galdin Raphael <gldraphael@gmail.com>
2016-10-02 13:14:49 +0530

    Add code for tutorial 6
    - Using the SDL2_Image extension library

---
+++ 06/main.cpp
@@ -0,0 +1,181 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include <string>
+
+/*
+ * CONSTANTS
+ */
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+SDL_Rect stretchRect = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT}; // x, y, width, height
+
+/*
+ * FUNCTION DECLARATIONS
+ */
+
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+/*
+ * GLOBAL VARIABLES
+ */
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+    
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+//The image we will load and show on the screen
+SDL_Surface* gHelloWorld = NULL;
+
+bool init()
+{
+    //Initialization flag
+    bool success = true;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+        return false;
+    }
+    
+    //Create window
+    gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if(gWindow == NULL)
+    {
+        printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+        return false;
+    }
+    
+    //Initialize PNG loading
+    int imgFlags = IMG_INIT_PNG;
+    if(!(IMG_Init(imgFlags) & imgFlags))
+    {
+        printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+        return false;
+    }
+
+    //Get window surface
+    gScreenSurface = SDL_GetWindowSurface(gWindow);
+
+    return true;
+}
+
+SDL_Surface* loadSurface(std::string path)
+{
+    //The final optimized image
+    SDL_Surface* optimizedSurface = NULL;
+
+    //Load image at specified path
+    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+    if(loadedSurface == NULL)
+    {
+        printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+        return NULL;
+    }
+    
+    //Convert surface to screen format
+    optimizedSurface = SDL_ConvertSurface(loadedSurface, gScreenSurface->format, static_cast<unsigned int>(NULL));
+    if(optimizedSurface == NULL)
+    {
+        printf( "Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+    }
+
+    //Get rid of old loaded surface
+    SDL_FreeSurface(loadedSurface);
+    
+
+    return optimizedSurface;
+}
+
+
+bool loadMedia()
+{
+    //Loading success flag
+    bool success = true;
+
+    //Load splash image
+    gHelloWorld = loadSurface("image.bmp");
+    if(gHelloWorld == NULL)
+    {
+        printf("Unable to load image %s! SDL Error: %s\n", "image.bmp", SDL_GetError());
+        success = false;
+    }
+
+    return success;
+}
+
+void close()
+{
+    //Deallocate surface
+    SDL_FreeSurface(gHelloWorld);
+    gHelloWorld = NULL;
+
+    //Destroy window
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+
+    //Quit SDL subsystems
+    SDL_Quit();
+}
+
+int main(int argc, char* args[])
+{
+    //Main loop flag
+    bool quit = false;
+
+    //Event handler
+    SDL_Event e;
+
+    //Start up SDL and create window
+    if(!init())
+    {
+        printf("Failed to initialize!\n");
+    }
+    else
+    {
+        //Load media
+        if(!loadMedia())
+        {
+            printf("Failed to load media!\n");
+        }
+        else
+        {
+            while(!quit)
+            {
+                // Handle events on queue
+                while(SDL_PollEvent(&e) != 0)
+                {
+                    // User requests quit
+                    if(e.type == SDL_QUIT)
+                    {
+                        quit = true;
+                    }
+                }
+
+                // Apply the image
+                SDL_BlitScaled(gHelloWorld, NULL, gScreenSurface, &stretchRect);
+                // Update the surface
+                SDL_UpdateWindowSurface(gWindow);
+                // Wait 100ms
+                SDL_Delay(100);
+            }
+        }
+    }
+
+    //Free resources and close SDL
+    close();
+
+    return 0;
+}
